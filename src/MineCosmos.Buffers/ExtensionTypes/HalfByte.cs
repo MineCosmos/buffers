a8c1f499d1c49b5bdc25ddf9614619bfef39e765
Powers <49866401+DonPangPang@users.noreply.github.com>
2023-09-18 04:48:30 +0800

    添加HalfByte; update version;

---
+++ src/MineCosmos.Buffers/ExtensionTypes/HalfByte.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MineCosmos.Buffers;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct HalfByte
+{
+    private bool _b0;
+    private bool _b1;
+    private bool _b2;
+    private bool _b3;
+
+    public HalfByte(byte value)
+    {
+        _b0 = (byte)(value & 0000_0001) > 0;
+        _b1 = (byte)(value & 0000_0010) > 0;
+        _b2 = (byte)(value & 0000_0100) > 0;
+        _b3 = (byte)(value & 0000_1000) > 0;
+    }
+
+    public byte Value => GetValue();
+
+    private byte GetValue()
+    {
+        byte result = 0;
+
+        result = _b0 ? (byte)(result | 0000_0001) : result;
+        result = _b1 ? (byte)(result | 0000_0010) : result;
+        result = _b2 ? (byte)(result | 0000_0100) : result;
+        result = _b3 ? (byte)(result | 0000_1000) : result;
+
+        return result;
+    }
+}
